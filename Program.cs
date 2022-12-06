9bdee50ef18dd41fef5fcb027f9ca675deeb4e4a
Aman-ABJIMA <119652332+Aman-ABJIMA@users.noreply.github.com>
2022-12-06 17:44:12 +0530

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,27 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Enter Marks of Hindi:");
+int h=Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Enter Marks of Engilsh:");
+int e = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Enter Marks of Maths:");
+int m = Convert.ToInt32(Console.ReadLine());
+int total = h + e + m;
+double percentage=(total/3);
+Console.Clear();
+if (percentage >= 90F && percentage <= 100F)
+    Console.WriteLine("Grade: A");
+else if (percentage >= 80F && percentage <= 89F)
+    Console.WriteLine("Grade: B");
+else if (percentage >= 70F && percentage <= 79F)
+    Console.WriteLine("Grade: C");
+else if (percentage >= 60F && percentage <= 69F)
+    Console.WriteLine("Grade: D");
+else if (percentage >= 50F && percentage <= 59F)
+    Console.WriteLine("Grade: E");
+else Console.WriteLine("Failed!");
+Console.WriteLine("-----------");
+Console.WriteLine($"Hindi:  {h}\nEnglish:{e}\nMaths:  {m}");
+Console.WriteLine("------------");
+Console.WriteLine($"Obt Marks:{total}");    
+
+
